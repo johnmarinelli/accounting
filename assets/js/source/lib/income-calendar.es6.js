ec625a7ab0dce1db47d0b0fecef0e67369a81fb4
John Marinelli <john@Macs-MBP-3.att.net>
2016-04-26 07:55:51 -0700

    fixed day algorithm

--- assets/js/source/lib/income-calendar.es6.js
+++ assets/js/source/lib/income-calendar.es6.js
@@ -16,7 +16,7 @@ let buildCalendar = (DateFunctions) => {
       return Array.prototype.map.call(data.Categories, (e, i) => {
         return {
           category: e,
-          days: (data.Amounts[i] / monthlyIncome) * daysInMonth
+          days: (+((data.Amounts[i] / monthlyIncome) * daysInMonth).toFixed(2))
         };
       });
     }).call(this, d, 7500);
@@ -25,21 +25,80 @@ let buildCalendar = (DateFunctions) => {
 
     for (let i = 0; i < buckets.length; ++i) {
       let bucket = buckets[i],
-        bucketDays = bucket.days;
+        bucketDays = bucket.days,
+        bucketDayBoxes = [];
       let lastDayBox = dayBoxes[dayBoxes.length - 1];
 
       // the last day still has empty space
-      if (lastDayBox && lastDayBox.total.toFixed(2) < 100.00) {
-        let diff = Math.abs(lastDayBox.total - bucketDays);
+      if (lastDayBox && lastDayBox.total < 100.00) {
+        let bucketDays = bucket.days,
+            totalDays = lastDayBox.total + bucketDays,
+            over100 = (totalDays * 100) > 100.00;
+        console.log(lastDayBox);
+        console.log('bucket days: ' + bucketDays);
+        console.log('total days: ' + totalDays);
+
+        if (over100) {
+          console.log('over 100');
+          let distanceFrom100 = Math.abs(100.00 - lastDayBox.total),
+              leftoverBucketDays = Math.abs(bucketDays - (distanceFrom100 / 100));
+          console.log('leftover bucket days: ' + leftoverBucketDays);
+
+          console.log('distanceFrom100: '+ distanceFrom100);
+
+          dayBoxes[dayBoxes.length - 1].spending.push({
+            category: bucket.category,
+            percentage: distanceFrom100
+          });
+          dayBoxes[dayBoxes.length - 1].total = 100.00;
+
+          // insert the rest of it
+          for (let dayInc = lastDayBox.day; dayInc < lastDayBox.day + Math.floor(leftoverBucketDays); ++dayInc) {
+            bucketDayBoxes.push({ 
+              day: dayInc, 
+              spending: [
+                { category: bucket.category, percentage: 100.00 }
+              ],
+              total: 100.00
+            });
+          }
+        }
+        else {
+          dayBoxes[dayBoxes.length - 1].spending.push({
+            category: bucket.category,
+            percentage: bucket.days
+          });
+        }
       }
 
       // the last day is full or we're just starting
       else {
-        let bucketDayBoxes = [];
-        for (let j = 0; j < Math.ceil(days); ++j) {
-          
+        let lastDay = dayBoxes.length === 0 ? 0 : dayBoxes[dayBoxes.length - 1].day,
+            dayInc = lastDay + 1,
+            totalDays = bucket.days,
+            wholeDays = Math.floor(totalDays);
+        
+        for (; dayInc < lastDay + wholeDays; ++dayInc) {
+          bucketDayBoxes.push({ 
+            day: dayInc, 
+            spending: [
+              { category: bucket.category, percentage: 100.00 }
+            ],
+            total: 100.00
+          });
         }
+
+        let leftover = (totalDays - wholeDays) * 100;
+        bucketDayBoxes.push({
+          day: dayInc,
+          spending: [
+            { category: bucket.category, percentage: +(leftover.toFixed(2)) }
+          ],
+          total: leftover 
+        });
       }
+      dayBoxes = Array.prototype.concat.call(dayBoxes, bucketDayBoxes);
+      console.log(dayBoxes);
     }
   };
 
